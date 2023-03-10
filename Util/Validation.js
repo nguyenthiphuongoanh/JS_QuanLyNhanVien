2ea8df556f601002433d3637351306e9357a7f77
nguyenthiphuongoanh <phuongoanh.ssh11@gmail.com>
2023-03-10 16:04:45 +0700

    phần hoàn thành ban đầu

---
+++ Util/Validation.js
@@ -0,0 +1,84 @@
+function Validation () {
+    this.kiemTraRong = function (value,idError,name) {
+        if(value.trim() === ''){
+            document.getElementById(idError).innerHTML = `${name} không được bỏ trống !`;
+            return false;
+        }
+        document.getElementById(idError).innerHTML = '';
+        return true;
+    }
+    this.kiemTraKyTu = function (value,idError,name) {
+        var regexLetter = /^[A-Z a-z]+$/;
+        //Nếu chuỗi định dạng test thành công value thì true
+        if(regexLetter.test(value)){
+            document.getElementById(idError).innerHTML = ''
+            return true;
+        }
+        document.getElementById(idError).innerHTML = `${name} tất cả phải là ký tự`;
+        return false;
+    }
+    this.kiemTraEmail = function (value,idError,name) {
+        var regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(regexEmail.test(value)) {
+            document.getElementById(idError).innerHTML = '';
+            return true;
+        }
+        document.getElementById(idError).innerHTML = `${name} không hợp lệ!`;
+        return false;
+    }
+    this.kiemTraSo = function (value,idError,name) {
+        var regexNumber = /^[0-9]+$/;
+        if(regexNumber.test(value)){
+            document.getElementById(idError).innerHTML = '';
+            return true;
+        }
+        document.getElementById(idError).innerHTML = `${name} không hợp lệ!`;
+        return false;
+    }
+
+    this.kiemTraDoDai = function (value,idError,name,minLength,maxLength) {
+        // 'abcd'.length = 4
+        if(value.length > maxLength || value.length < minLength) {
+            document.getElementById(idError).innerHTML = `${name} từ ${minLength} đến ${maxLength} ký tự !`;
+            return false;
+        }
+        document.getElementById(idError).innerHTML = '';
+        return true;
+    }
+
+    this.kiemTraGiaTri = function (value,idError,name,minValue,maxValue) {
+        var regexNumber = /^[0-9.]+$/;
+        if(regexNumber.test(value)){
+            //Kiểm tra giá trị
+            if(Number(value) < minValue || Number(value) > maxValue) {
+                document.getElementById(idError).innerHTML = `${name} hợp lệ từ ${minValue} đến ${maxValue} !`;
+                return false;
+            }
+            document.getElementById(idError).innerHTML = '';
+            return true;
+        }
+        document.getElementById(idError).innerHTML = `${name} giá trị không hợp lệ !`;
+        return false;
+    }
+
+    this.kiemTraMatKhau = function (value,idError,name) {
+        var regexPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,10}$/;
+        if(regexPass.test(value)) {
+            document.getElementById(idError).innerHTML = '';
+            return true;
+        }
+        document.getElementById(idError).innerHTML = `${name}  từ 6-10 ký tự (chứa ít nhất 1 ký tự số, 1 ký tự in hoa, 1 ký tự đặc biệt) !`;
+        return false;
+    }
+
+    this.kiemTraDinhDangNgay = function (value,idError,name) {
+        var dateformat  =  /^(0?[1-9]|1[0-2])[\/](0?[1-9]|[1-2][0-9]|3[01])[\/]\d{4}$/;
+        if (value.match(dateformat)) {
+            document.getElementById(idError).innerHTML = '';
+            return true;
+        }
+        document.getElementById(idError).innerHTML = `${name} chưa đúng định dạng mm/dd/yyyy!`;
+        return false;
+    }
+
+}
---
+++ models/NhanVien.js
@@ -0,0 +1,13 @@
+
+function NhanVien () {
+    this.taiKhoanNhanVien = '';
+    this.tenNhanVien = '';
+    this.email = '';
+    this.matKhau ='';
+    this.ngayLam = '';
+    this.luongCB = '';
+    this.chucVu = '';
+    this.gioLam = '';
+    this.tongLuong = '';
+    this.loaiNhanVien = '';
+}
